ce10f787f8b7838cef8324506e88979be81cd4b7
KyaMovVM <91567556+KyaMovVM@users.noreply.github.com>
2025-07-02 06:16:30 +0500

    Исправлен заголовок Permissions-Policy

--- setup_django.sh
+++ setup_django.sh
@@ -137,7 +137,7 @@ sudo bash -c "cat > $APACHE_SSL_CONF" <<EOV
     Header always set X-Frame-Options "SAMEORIGIN"
     Header always set X-Content-Type-Options "nosniff"
     Header always set Referrer-Policy "strict-origin-when-cross-origin"
-    Header always set Permissions-Policy "geolocation=(), microphone()"
+    Header always set Permissions-Policy "geolocation=(), microphone=()"
     Header always set Content-Security-Policy "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'"
 
     WSGIDaemonProcess ${PROJECT_NAME} user=www-data group=www-data python-path=${PROJECT_DIR}
